f09fe0e3467e29fe15d400937992b448a5d88d2d
Lion <815694355@qq.com>
2016-05-11 23:11:28 +0800

    1、areas.json 文件中不全港澳台信息，并增加 sensitive_areas 以示区分 2、ChinaCity的list、get和data方法改造

--- lib/china_city.rb
+++ lib/china_city.rb
@@ -6,13 +6,17 @@ module ChinaCity
   PATTERN = /(\d{2})(\d{2})(\d{2})/
 
   class << self
-    def list(parent_id = '000000')
+    # @options[:show_all] 是否显示港澳台这三处敏感地区
+    def list(parent_id, options = {})
+      parent_id ||= '000000'
+      show_all = options[:show_all] || false
+
       result = []
       return result if parent_id.blank?
       province_id = province(parent_id)
       city_id     = city(parent_id)
       district_id = district(parent_id)
-      children = data
+      children = data show_all
       children = children[province_id][:children] if children.has_key?(province_id)
       children = children[city_id][:children] if children.has_key?(city_id)
       children = children[district_id][:children] if children.has_key?(district_id)
@@ -29,7 +33,8 @@ module ChinaCity
     def get(id, options = {})
       return '' if id.blank?
       prepend_parent = options[:prepend_parent] || false
-      children = data
+      # 补全areas.json中的数据后，可兼容港澳台下的children信息
+      children = data true
       return children[id][:text] if children.has_key?(id)
       province_id = province(id)
       province_text = children[province_id][:text]
@@ -58,57 +63,49 @@ module ChinaCity
       code[0..5].rjust(6,'0')
     end
 
-    def data
-      unless @list
-        #{ '440000' =>
-        #  {
-        #    :text => '广东',
-        #    :children =>
-        #      {
-        #        '440300' =>
-        #          {
-        #            :text => '深圳',
-        #            :children =>
-        #              {
-        #                '440305' => { :text => '南山' }
-        #              }
-        #           }
-        #       }
-        #   }
-        # }
-        @list = {}
-        #@see: https://github.com/cn/GB2260
-        json = JSON.parse(File.read("#{Engine.root}/db/areas.json"))
-        streets = json.values.flatten
-        streets.each do |street|
-          id = street['id']
-          text = street['text']
-          if id.size == 6    # 省市区
-            if id.end_with?('0000')                           # 省
-              @list[id] =  {:text => text, :children => {}}
-            elsif id.end_with?('00')                          # 市
-              province_id = province(id)
-              @list[province_id] = {:text => nil, :children => {}} unless @list.has_key?(province_id)
-              @list[province_id][:children][id] = {:text => text, :children => {}}
-            else
-              province_id = province(id)
-              city_id     = city(id)
-              @list[province_id] = {:text => text, :children => {}} unless @list.has_key?(province_id)
-              @list[province_id][:children][city_id] = {:text => text, :children => {}} unless @list[province_id][:children].has_key?(city_id)
-              @list[province_id][:children][city_id][:children][id] = {:text => text, :children => {}}
-            end
-          else               # 街道
+    def data show_all=false
+      @list_all ||= list_data true
+      @list ||= list_data
+
+      show_all ? @list_all : @list
+    end
+
+    def list_data show_all=false
+      list = {}
+      #@see: https://github.com/cn/GB2260
+      json = JSON.parse(File.read("#{Engine.root}/db/areas.json"))
+      streets = json.values.flatten
+      streets.each do |street|
+        # skip sensitive areas when show_all is false
+        next if (!show_all && street['sensitive_areas'])
+
+        id = street['id']
+        text = street['text']
+        if id.size == 6    # 省市区
+          if id.end_with?('0000')                           # 省
+            list[id] =  {:text => text, :children => {}}
+          elsif id.end_with?('00')                          # 市
+            province_id = province(id)
+            list[province_id] = {:text => nil, :children => {}} unless list.has_key?(province_id)
+            list[province_id][:children][id] = {:text => text, :children => {}}
+          else
             province_id = province(id)
             city_id     = city(id)
-            district_id = district(id)
-            @list[province_id] = {:text => text, :children => {}} unless @list.has_key?(province_id)
-            @list[province_id][:children][city_id] = {:text => text, :children => {}} unless @list[province_id][:children].has_key?(city_id)
-            @list[province_id][:children][city_id][:children][district_id] = {:text => text, :children => {}} unless @list[province_id][:children][city_id][:children].has_key?(district_id)
-            @list[province_id][:children][city_id][:children][district_id][:children][id] = {:text => text}
+            list[province_id] = {:text => text, :children => {}} unless list.has_key?(province_id)
+            list[province_id][:children][city_id] = {:text => text, :children => {}} unless list[province_id][:children].has_key?(city_id)
+            list[province_id][:children][city_id][:children][id] = {:text => text, :children => {}}
           end
+        else               # 街道
+          province_id = province(id)
+          city_id     = city(id)
+          district_id = district(id)
+          list[province_id] = {:text => text, :children => {}} unless list.has_key?(province_id)
+          list[province_id][:children][city_id] = {:text => text, :children => {}} unless list[province_id][:children].has_key?(city_id)
+          list[province_id][:children][city_id][:children][district_id] = {:text => text, :children => {}} unless list[province_id][:children][city_id][:children].has_key?(district_id)
+          list[province_id][:children][city_id][:children][district_id][:children][id] = {:text => text}
         end
       end
-      @list
+      list
     end
 
     def match(code)
